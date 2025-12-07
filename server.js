f14f33e34172476f1be8ad09ad1dab3260efa7aa
cdoeamit <leeksyou18@gmail.com>
2025-12-07 10:42:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://silveer-forntend.vercel.app/',
+  origin: 'https://silveer-forntend.vercel.app',
   credentials: true
 }));
 
